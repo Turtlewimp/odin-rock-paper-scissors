1fe81a52cbc2beaddedcdc71641d8f531d40a5dc
Turtlewimp <turtlewimp@gmail.com>
2023-01-31 18:08:01 -0800

    Add points counters and counter reset for after 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
-            return "rock";
+            return "Rock";
         case 1:
-            return "paper";
+            return "Paper";
         case 2:
-            return "scissors";
+            return "Scissors";
         default:
             break;
     }
 }
 
-//const div = document.querySelector('#displayBoard');
-//div.textContent = playerSelection + computerSelection;
-
+let playerPointCounter = 0;
+let computerPointCounter = 0;
 let clickCounter = 0;
+
 function playRound(playerSelection, computerSelection) {
     const playerPoints = document.querySelector('#playerPoints');
     const computerPoints = document.querySelector('#computerPoints');
@@ -22,111 +22,31 @@ function playRound(playerSelection, computerSelection) {
     const commentary = document.querySelector('#commentary');
 
     //update round display
-    clickCounter ++;
-    round.textContent = `Round: ${clickCounter} of 5`;
-    let playCombo = playerSelection.concat(computerSelection);
+    round.textContent = `Round: ${++clickCounter} of 5`;
+
+    //reset after 5 rounds played
+    if (clickCounter%6 === 0) {
+        commentary.textContent = `That's game! The final score was ${playerPointCounter} to ${computerPointCounter}. Click the buttons to play again.`;
+        playerPoints.textContent = `Your Points: ${playerPointCounter = 0}`;
+        computerPoints.textContent = `Computer's Points: ${computerPointCounter = 0}`;
+        round.textContent = `Round: ${clickCounter = 0} of 5`;
+        return;
+    } 
 
     //compare for play outcome
-    if (playerSelection == computerSelection) {
+    let playCombo = playerSelection.concat(computerSelection);
+    if (playerSelection === computerSelection) {
         commentary.textContent = `${playerSelection} ties ${computerSelection}`;
-        return console.log("tie");
     }
-    else if (playCombo == "rockscissors" || playCombo == "paperrock" || playCombo == "scissorspaper") {
-        //update playerPoints ++
+    else if (playCombo === "RockScissors" || playCombo === "PaperRock" || playCombo === "ScissorsPaper") {
+        playerPoints.textContent = `Your Points: ${++playerPointCounter}`;
         commentary.textContent = `${playerSelection} beats ${computerSelection}`;
-        return console.log("win");
     }
-    else if (playCombo == "rockpaper" || playCombo == "paperscissors" || playCombo == "scissorsrock"){
-        //update computerPoints ++
+    else if (playCombo === "RockPaper" || playCombo === "PaperScissors" || playCombo === "ScissorsRock"){
+        computerPoints.textContent = `Computer's Points: ${++computerPointCounter}`;
         commentary.textContent = `${computerSelection} beats ${playerSelection}`;
-        return console.log("lose");
-    }
+    } 
     else {
-        return console.log("ERROR");
-    }
-
-
-
-}
-
-function displayUpdate() {
-
-
-    //checks div#round 
-    //if round changes from 4 to 5
-    //update playbyplay with final score and game
-    //"you win" "you lose" "it's a tie"
-    //button labels all change to reset
-    //"click the button again to reset"
-
-    //if round changes from 5 to 6 then
-    //immediately reset all stats
-}
-
-
-
-/*
-//play 5 rounds of rock paper scissors
-function game() {
-    
-    //declare variables
-    let playerChoice;
-    let computerChoice;
-    let playOutcome;
-    let playerPoints = 0;
-    let computerPoints = 0;
-
-    //print initial scores
-    console.log(`The score is ${playerPoints} to ${computerPoints}.`);
-
-    //play 5 rounds
-    for (let i = 1; i<6; i++) {
-        //declare round
-        console.log("Round " + i);
-        
-        
-        //playerChoice = prompt().toLowerCase();
-           
-
-        //print out player choice
-        console.log("You: " + playerChoice);
-
-        //get and print computer choice
-        computerChoice = getComputerChoice();
-        console.log("Opponent: " + computerChoice);
-
-        switch (playOutcome = playRound(playerChoice, computerChoice)) {
-            case "tie":
-                console.log(`${playerChoice} ties ${computerChoice}`);
-                break;
-            case "win":
-                playerPoints++;
-                console.log(`${playerChoice} beats ${computerChoice}`);
-                break;
-            case "lose":
-                computerPoints++;
-                console.log(`${computerChoice} beats ${playerChoice}`);
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    //print final score
-    console.log(`The final score is ${playerPoints} to ${computerPoints}.`)
-    //declare winner
-    if (playerPoints == computerPoints) {
-        console.log("No one wins; No one loses.");
-    }
-    else if (playerPoints > computerPoints) {
-        console.log("You win the game!");
-    }
-    else {
-        console.log("You lose the game!");
+        return console.log("ERROR in playRound()");
     }
 }
-*/
-
-//if game ends, prompt user to play again
-
